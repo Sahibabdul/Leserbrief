ae22deb29df1c4583365c9c84c7d0e597285b80b
Sahibabdul <trump69trump@gmail.com>
2019-12-24 01:40:38 +0100

    ree

---
+++ main.py
@@ -0,0 +1,45 @@
+from leserbrief import Leserbrief
+import os
+
+text = open("text.txt", "w")
+authors = open("authors.txt", "w")
+title = open("title.txt", "w")
+text.write("")
+authors.write("")
+title.write("")
+text.close()
+authors.close()
+title.close()
+
+base_url = "https://www.volksblatt.li/Leserbriefe"
+amount_of_letters = 647505
+newest_letter_id = 647505
+list_of_letters=[]
+blacklist = open("blacklist.txt","r")
+list_black = set(blacklist.read().split())
+blacklist.close()
+
+
+#--------Creator--------
+text = open("text.txt", "a")
+blacklist = open("blacklist.txt", "a")
+authors = open("authors.txt", "a")
+title = open("title.txt", "a")
+for i in range(amount_of_letters):
+    if str(newest_letter_id-i) not in list_black:
+        letter=Leserbrief(newest_letter_id-i)
+    else:
+        continue
+    print(letter.id)
+    if letter.get_text()=="Error":
+        blacklist.write(letter.id+"\n")
+        print("Fail")
+    else:
+        text.write( letter.id+" --- "+letter.get_text()+"\n")
+        authors.write(letter.id+" --- "+ letter.get_creator()+"\n")
+        title.write(letter.id+" --- "+ letter.get_title()+"\n")
+    
+text.close()
+blacklist.close()
+authors.close()
+title.close()
