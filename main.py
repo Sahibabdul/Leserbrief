3b8d4185b69a66e1fabd1d9df60c101e2b513831
Philipp Marock <44873758+Sahibabdul@users.noreply.github.com>
2020-04-19 22:45:22 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -8,15 +8,10 @@ logging.basicConfig(filename='debug.log', format='%(levelname)s:%(message)s | %(
 #Analyze Config
 #Base URL atm it only works for this website but the Code could be adapted to other sites
 base_url = "https://www.volksblatt.li/Leserbriefe"
-<<<<<<< HEAD
 #Bounds for Letters to analyze
 amount_of_letters = 653280
 lower_bound = 647504
 logging.info("Upper bound:" +str(amount_of_letters)+" Lower bound:"+str(lower_bound))
-=======
-amount_of_letters = 653371
-lower_bound = 647504
->>>>>>> b05ec2c4e18c6cde865a82a819108fecea97b151
 list_of_letters=[]
 #Looking at all letters already looked at
 with open("text.txt","r") as articles:
@@ -35,7 +30,6 @@ with open("text.txt","a") as text:
     #Going from upperbound to Lower Bound
     for i in reversed(range(newest_letter_id)):
         if i < lower_bound:
-<<<<<<< HEAD
             logging.debug("Exited with: "+str(i))
             exit()
         letter = Leserbrief(i)
@@ -45,12 +39,3 @@ with open("text.txt","a") as text:
             text.write( letter.id+" --- "+letter.get_title() + " --- "+ letter.get_creator()+" --- "+ letter.get_text()+"\n")
             logging.info("")
             print("Done with: "+str(letter.id))
-=======
-            exit()
-        letter = Leserbrief(i)
-        if letter.get_text()=="Error":
-            print("Fail " + letter.id)
-        else:
-            text.write( letter.id+" --- "+letter.get_title() + " --- "+ letter.get_creator()+" --- "+ letter.get_text()+"\n")
-            print("Success " + letter.id)
->>>>>>> b05ec2c4e18c6cde865a82a819108fecea97b151
